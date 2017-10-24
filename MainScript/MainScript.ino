6d711386b87393655e89bec6c5165f0fa8039740
Michael Yang <michaelyang42@gmail.com>
2017-10-23 23:45:44 -0700

    started data logging

--- MainScript/MainScript.ino
+++ MainScript/MainScript.ino
@@ -3,10 +3,29 @@
 #include <Wire.h>
 #include <Adafruit_BNO055.h>
 #include <utility/imumaths.h>
+#include <TinyGPS++.h>
 
-
+//Log
 File flightLog;
+
+//orientation sensor
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
+
+
+//=====================GPS=====================
+// A sample NMEA stream.
+const char *gpsStream =
+  "$GPRMC,045103.000,A,3014.1984,N,09749.2872,W,0.67,161.46,030913,,,A*7C\r\n"
+  "$GPGGA,045104.000,3014.1985,N,09749.2873,W,1,09,1.2,211.6,M,-22.5,M,,0000*62\r\n"
+  "$GPRMC,045200.000,A,3014.3820,N,09748.9514,W,36.88,65.02,030913,,,A*77\r\n"
+  "$GPGGA,045201.000,3014.3864,N,09748.9411,W,1,10,1.2,200.8,M,-22.5,M,,0000*6C\r\n"
+  "$GPRMC,045251.000,A,3014.4275,N,09749.0626,W,0.51,217.94,030913,,,A*7D\r\n"
+  "$GPGGA,045252.000,3014.4273,N,09749.0628,W,1,09,1.3,206.9,M,-22.5,M,,0000*6F\r\n";
+
+// The TinyGPS++ object
+TinyGPSPlus gps;
+
+byte gps_set_success = 0; 
 
 void setup(void) 
 {
@@ -23,13 +42,15 @@ void setup(void)
   }
   delay(1000); 
   bno.setExtCrystalUse(true);
+
+  //Initialize GPS
+  setupGPS();
 }
 
 void loop(void) 
 {
+  /* Display the orientation data */
   imu::Vector<3> euler = bno.getVector(Adafruit_BNO055::VECTOR_EULER);
-  
-  /* Display the floating point data */
   Serial.print("X: ");
   Serial.print(euler.x());
   Serial.print(" Y: ");
@@ -37,6 +58,9 @@ void loop(void)
   Serial.print(" Z: ");
   Serial.print(euler.z());
   Serial.println("");
+
+  //display GPS data
+  displayInfo();
   
   delay(100);
 }
@@ -45,3 +69,89 @@ void logToSD() {
 }
 
 
+//=============GPS======================
+
+void setupGPS()
+{
+  Serial.begin(9600);
+  Serial3.begin(9600); 
+  //This is how we set flight mode 
+  Serial.println("Setting uBlox nav mode: ");
+  uint8_t setNav[] = {
+    0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00,
+    0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC };
+
+    sendUBX(setNav, 44);
+
+  Serial.println(F("BasicExample.ino"));
+  Serial.println(F("Basic demonstration of TinyGPS++ (no device needed)"));
+  Serial.print(F("Testing TinyGPS++ library v. ")); Serial.println(TinyGPSPlus::libraryVersion());
+  Serial.println(F("by Mikal Hart"));
+  Serial.println();
+
+  while (*gpsStream)
+    if (gps.encode(*gpsStream++))
+      displayInfo();
+
+  Serial.println();
+  Serial.println(F("Done."));
+}
+
+void displayInfo()
+{
+  Serial.print(F("Location: ")); 
+  if (gps.location.isValid())
+  {
+    Serial.print(gps.location.lat(), 6);
+    Serial.print(F(","));
+    Serial.print(gps.location.lng(), 6);
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F("  Date/Time: "));
+  if (gps.date.isValid())
+  {
+    Serial.print(gps.date.month());
+    Serial.print(F("/"));
+    Serial.print(gps.date.day());
+    Serial.print(F("/"));
+    Serial.print(gps.date.year());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.print(F(" "));
+  if (gps.time.isValid())
+  {
+    if (gps.time.hour() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.hour());
+    Serial.print(F(":"));
+    if (gps.time.minute() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.minute());
+    Serial.print(F(":"));
+    if (gps.time.second() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.second());
+    Serial.print(F("."));
+    if (gps.time.centisecond() < 10) Serial.print(F("0"));
+    Serial.print(gps.time.centisecond());
+  }
+  else
+  {
+    Serial.print(F("INVALID"));
+  }
+
+  Serial.println();
+}
+void sendUBX(uint8_t* MSG, uint8_t len) {
+  for(int i = 0; i < len; i++) {
+    Serial1.write(MSG[i]);
+    Serial.print(MSG[i], HEX);
+  }
+  Serial1.println();
+}
