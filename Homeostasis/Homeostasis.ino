bd3a7e48879e0227555882a59f33ef1c1fc78c2c
Michael Yang <michaelyang42@gmail.com>
2017-10-27 00:28:28 -0700

    started heater control script

---
+++ Homeostasis/Homeostasis.ino
@@ -0,0 +1,16 @@
+int heaterControlPin = 23; //TODO: make sure this doesn't collide with anything else
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
+
+void manageHeaters(double currentTemp) {
+  
+}
+
