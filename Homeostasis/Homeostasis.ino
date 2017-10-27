c3cb021793f170573dfdbbf5c9d36935b3922631
Michael Yang <michaelyang42@gmail.com>
2017-10-27 01:17:54 -0700

    wrote Heater test sketch

--- Homeostasis/Homeostasis.ino
+++ Homeostasis/Homeostasis.ino
@@ -1,16 +1,36 @@
 int heaterControlPin = 23; //TODO: make sure this doesn't collide with anything else
 
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  pinMode(heaterControlPin, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("Temp = 10C...");
+  manageHeaters(10);
+  delay(5000);
+  Serial.println("Temp = 0C...");
+  manageHeaters(0);
+  delay(5000);
+  Serial.println("Temp = -5C...");
+  manageHeaters(-5);
+  delay(5000);
+  Serial.println("Temp = -15C...");
+  manageHeaters(-15);
+  delay(5000);
 }
 
+/* Expects current temperature in Celsius,
+ * turns on heaters if temp <= 0C, using proportional control to turn on heaters more
+ * if the current temperature is further from 0C (maximum acheived at -10C).
+*/
 void manageHeaters(double currentTemp) {
-  
+  if (currentTemp < 0) {
+    double p = min(1, max(0, currentTemp / -10));
+    Serial.print("p=");
+    Serial.print(p);
+    Serial.println();
+    analogWrite(heaterControlPin, 255. * p);
+  }
 }
 
